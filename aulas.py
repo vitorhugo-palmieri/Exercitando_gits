1654eab0fd2cc18f9c46181212555cb7b21ebfee
Vitor Hugo <vitor.palmieri@outlook.com>
2021-11-30 13:51:20 -0300

    vscode vitor hugo

--- aulas.py
+++ aulas.py
@@ -2,4 +2,5 @@ print("Testando o git")
 print("Praticando o envio de commits ao git hub")
 print("Testand commit pelo vscode") 
 #testando enviar por outra pessoa
-#enviando por outra pessoa pelo vscode
+#enviando por outra pessoa pelo vscode 
+#querendo enviar para github
