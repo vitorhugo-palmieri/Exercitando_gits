0744932cab3f76bfc0baf1385d22123b18236bf2
Vitor Hugo <vitor.palmieri@outlook.com>
2021-11-30 12:13:09 -0300

    Comitando depois de mudar de pasta

---
+++ aulas.py
@@ -0,0 +1,105 @@
+# ////////////22/11/2021
+
+# nomes=["Vitor Hugo","Silvana","Tais"]
+# nomes2=["Gabriel","Luana","Reinaldo"]
+
+# totalnomes=nomes+nomes2
+
+#/////////////23/11/2021
+
+# lista=[1,"Paulo",3]
+
+# for i in range(3):
+#     lista[i]=i
+
+# print(lista)
+
+# for num in lista:
+#     print(num)
+
+# lista1=[1,2,3,4,5,6,7,8,9,10]
+# lista2=[1,2,3,4,5,6,7,8,9,10]
+# lista3=[0]*10
+
+# for i in range(len(lista1)):
+#     lista3[i]=lista1[i]*lista2[i]
+
+
+# print(lista3)
+
+# for i in lista1:
+#     print(i)
+
+# lista=[]
+# for i in range(2,11,2):
+#     lista.append(i)
+
+# print(lista) 
+ 
+# lista=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15] 
+
+
+# for i in range(13,-1,-2):
+  
+#    lista.pop(i)
+
+# print(lista)
+
+# sempre=[1,1,1,1]
+# frenquentemente=[1,1,1,1,1,1,1,1]
+# ocasionalmente=[1,1,1,1,1]
+# raramente=[1,1,1]
+# nunca=[]
+
+# lista1=[0,1,2,4,4,5,6,7,8]
+# lista2=[3,4,5,6,7,8,9,10,11]
+# lista3=[0]*9
+
+# print(len(lista1),len(lista2))
+
+# for i in lista1:
+#     lista3[i]=lista1[i]+lista2[i]
+
+# print(lista3)
+
+#///24/11/2021
+
+# sentenca=["jacare"]
+
+
+# animais=["gato","cachorro","pato","galinha","cobra","jacare","aguia"]
+# copia_animais=[]
+# for i in animais:
+#     if "g" in i:
+#         copia_animais.append(i)
+
+# for i in range(7):
+#     print(animais[i])
+
+# lista=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31]
+# nova_lista=[]
+# for i in range(0,len(lista)-1,2):
+#     print(lista[i])
+
+############################################25/11/2021
+
+# pilha=[1,2,3,5,6]
+
+# def contaElemento (pilha):
+#     print(len(pilha))
+
+# def verifica_vazio(pilha):
+#     if len(pilha)==0:
+#         print("A pilha esta vazia")
+#     else:
+#         print("A pilha tem elementos")
+
+
+# verifica_vazio(pilha)
+# contaElemento(pilha)
+
+#################### 29/11/2021
+    
+
+
+
